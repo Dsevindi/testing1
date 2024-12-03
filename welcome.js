a58b0843c48881f8badc6fb0101c96275b348a0c
Dsevindi <dspannilavithana@gmail.com>
2024-12-03 21:14:21 +0530

    Create welcome.js

---
+++ welcome.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <script defer src="welcome.js"></script>
+</head>
+<body>
+    <h1 id="welcomeMessage"></h1>
+    <button id="logoutButton">Logout</button>
+</body>
+</html>
